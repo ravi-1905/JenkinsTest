73db39250391de9c442f7323766ab49e53d9e6df
ravi-1905 <ravikumarab33@gmail.com>
2024-10-29 09:04:36 +0530

    maven commit to main branch

--- src/main/java/JenkinsTest.java
+++
@@ -1,7 +0,0 @@
-package src.main.java;
-
-public class JenkinsTest {
-    public static void main(String[] args) {
-        System.out.println("Jenkins Test");
-    }
-}
---
+++ src/test/java/JenkinsTest.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+
+    @Test
+    public void jenkinsTest1(){
+        System.out.println("Jenkins Test1");
+    }
+
+}
